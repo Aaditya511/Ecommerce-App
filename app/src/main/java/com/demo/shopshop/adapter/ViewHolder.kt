73642ffafbe83c4e9e7001bd821ebd45b246fcc3
Aditya <Aadityarock511@gmail.com>
2021-12-15 17:48:30 +0530

    Msg Commit

--- app/src/main/java/com/demo/shopshop/adapter/ViewHolder.kt
+++ app/src/main/java/com/demo/shopshop/adapter/ViewHolder.kt
@@ -2,8 +2,11 @@ package com.demo.shopshop
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val  bookImg = itemView.findViewById<ImageView>(R.id.imageViewBook)
+    val bookName = itemView.findViewById<TextView>(R.id.textViewBookName)
+    val authorName = itemView.findViewById<TextView>(R.id.authorName)
 }
--- app/src/main/java/com/demo/shopshop/ui/MainActivity.kt
+++ app/src/main/java/com/demo/shopshop/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.demo.shopshop.adapter.AdapterForCategoryRecylerView
 import com.demo.shopshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.recyclerView.addItemDecoration(
             object : RecyclerView.ItemDecoration() {
                 override fun getItemOffsets(
@@ -31,9 +33,26 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         )
-        binding.recyclerView.adapter = RecyclerViewAdapter(this)
+        binding.categoryRecylerView.addItemDecoration(
+            object : RecyclerView.ItemDecoration() {
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    super.getItemOffsets(outRect, view, parent, state)
+                    outRect.right = 50
+                    if(parent.getChildAdapterPosition(view) == 0)
+                        outRect.left = 50
+                }
+            }
+        )
 
+        binding.recyclerView.adapter = RecyclerViewAdapter(this)
+        binding.categoryRecylerView.adapter = AdapterForCategoryRecylerView(this)
 
 
     }
+
 }
