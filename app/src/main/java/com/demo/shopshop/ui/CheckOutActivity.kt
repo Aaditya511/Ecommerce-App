cb16b8c825996967c863d4ff9d33fac86807cb0d
Aditya <Aadityarock511@gmail.com>
2021-12-17 15:03:29 +0530

    Msg Commit

--- app/src/main/java/com/demo/shopshop/ui/CheckOutActivity.kt
+++ app/src/main/java/com/demo/shopshop/ui/CheckOutActivity.kt
@@ -3,18 +3,42 @@ package com.demo.shopshop.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import com.demo.shopshop.R
 import com.demo.shopshop.databinding.ActivityCheckOutBinding
+import kotlin.properties.Delegates
 
 class CheckOutActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCheckOutBinding
+    var quantity: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCheckOutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        quantity = binding.textViewQuantity.text.toString().toInt()
+
         binding.backBtnImage.setOnClickListener {
             val intent = Intent(this, BookOpenActivity::class.java)
             startActivity(intent)
         }
+
+        binding.minusIcon.setOnClickListener {
+            if (quantity == 1){
+                Toast.makeText(this, "U can Not", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                quantity--
+                binding.textViewQuantity.text = quantity.toString()
+            }
+
+        }
+        binding.plusIcon.setOnClickListener {
+            quantity++
+            binding.textViewQuantity.text = quantity.toString()
+
+        }
     }
+
 }
--- app/src/main/java/com/demo/shopshop/ui/MainActivity.kt
+++ app/src/main/java/com/demo/shopshop/ui/MainActivity.kt
@@ -3,9 +3,12 @@ package com.demo.shopshop
 import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.widget.Adapter
 import android.widget.BaseAdapter
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,20 +18,33 @@ import com.demo.shopshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var imgArr: Array<Int>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        imgArr = arrayOf(
+            R.drawable.rectanglefourthree,
+            R.drawable.rectanglefourtwo,
+            R.drawable.rectanglethreefour,
+            R.drawable.ellipse,
+            R.drawable.ellipsefour,
+            R.drawable.rectanglethreefour
+        )
+        runable()
 
         binding.recyclerView.addItemDecoration(
-            itemDecortion())
+            itemDecortion()
+        )
 
         binding.categoryRecylerView.addItemDecoration(
-            itemDecortion())
-
+            itemDecortion()
+        )
         binding.recyclerView.adapter = RecyclerViewAdapter(this)
         binding.categoryRecylerView.adapter = AdapterForCategoryRecylerView(this)
     }
+
 
     fun itemDecortion(): RecyclerView.ItemDecoration {
         return object : RecyclerView.ItemDecoration() {
@@ -45,9 +61,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
- /*   fun setUpRecyclerView(recyclerView: RecyclerView){
-        recyclerView.addItemDecoration(itemDecortion())*/
 
+    fun runable(){
+        val handler: Handler = Handler()
+        val run = object : Runnable {
+            override fun run() {
+                val randomNumber = (0..imgArr.size - 1).random()
+                handler.postDelayed(this, 86400000)
+                binding.todayOfferImage.setImageResource(imgArr.get(randomNumber))
 
+            }
+
+        }
+        handler.post(run)
     }
+    }
+
+
 
